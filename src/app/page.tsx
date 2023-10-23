c46851aeca519d66d895afda3a33a5f06615482e
Hirochi <hirochi@micros.co.jp>
2023-10-23 10:03:26 +0900

    modify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Home({
        */}
       <h1>以下はappディレクトリ配下のコンポーネント</h1>
       <Link href={{ pathname: "/excomp", query: searchParams }}>excomp</Link>
-      <Link href={"/excomp2"}>excomp2</Link>
+      <Link href={{ pathname: "/excomp2", query: searchParams }}>excomp2</Link>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
