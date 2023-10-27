8b71c6eba9a419e42f5ea2ead2b9cfae0fcd12cb
Hirochi <hirochi@micros.co.jp>
2023-10-27 10:10:59 +0900

    modify

--- src/app/api/foo/route.ts
+++ src/app/api/foo/route.ts
@@ -5,14 +5,7 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   console.log("GET request", searchParams.get("name"));
 
-  // const res = await fetch(
-  //   "https://p903-functions-nodetest-2.azurewebsites.net/api/HttpTrigger1?code=3sMQNrPK5XW-G7Q_o_xkGCPw0wMIyKEGYaEFeHGG7yhYAzFuO5irCQ==",
-  //   { next: { revalidate: 300 } }
-  // );
-  // const message = await res?.text();
-
   return NextResponse.json({ method: "GET" });
-  // return NextResponse.json({ method: "GET", message });
   // return new Response(JSON.stringify({ message: "Hello World" }));
 }
 
